82bdd54e47f8ec324dede833df7487f25ac17ada
faikarda <112086875+faikarda@users.noreply.github.com>
2025-01-17 10:44:30 +0000

    agents in main.py

--- agents.py
+++ agents.py
@@ -1,11 +1,3 @@
-from student import Student
-from embassy import Embassy
-from embassy_appointment_request_event import EmbassyAppointmentRequestEvent
-from appointment_confirmation_event import AppointmentConfirmationEvent
-
-
-
-
 class BaseAgent:
     """Base class for all agents."""
     def __init__(self, name):
@@ -64,29 +56,4 @@ if __name__ == "__main__":
     print(manager.handle_request("advisor", "machine learning"))
     print(manager.handle_request("event_coordinator", "technology"))
     print(manager.handle_request("mentor", "data science"))
-    print(manager.handle_request("resource_locator", "career development")) 
-
-
-
-
-
-event_queue = []
-
-# Example Usage
-student1 = Student("Piotr1", "Brudny", '1.02.1984', 'Ankara', '5435345345', 'ED4234323', event_queue)
-polish_embassy = Embassy('Polish Embassy', 'Ankara, Harika 10', '343242344', 'polishembassy@gov.tr', event_queue)
-
-
-student1.ask_for_embassy_appointment('10.12.2024')
-
-
-while event_queue: # it runs as long as there is an event in the list
-    event = event_queue.pop(0) # takes the event from the top
-
-    # matches events with the handler
-    if isinstance(event, EmbassyAppointmentRequestEvent):
-        polish_embassy.handle_appointment_request(event) 
-    elif isinstance(event, AppointmentConfirmationEvent):
-        student1.handle_appointment_confirmation(event)
-
-    
+    print(manager.handle_request("resource_locator", "career development"))
